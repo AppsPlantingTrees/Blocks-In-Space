bdbf575c9c9dfa00a9ea9983ada8fa54ef3cec20
randomUsername15 <63813435+randomUsername15@users.noreply.github.com>
2020-07-18 13:05:00 +0200

    Updating About canvas
    Working links, credits, link for Privacy Policy

--- Assets/Scripts/CanvasAbout.cs
+++ Assets/Scripts/CanvasAbout.cs
@@ -1,58 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
+using TMPro;
 
 public class CanvasAbout : MonoBehaviour
 {
-  /*string OUR_WEBSITE = "https://AppsPlantingTrees.org";
-  string TWITTER_PAGE_ID = "https://twitter.com/AppsTrees";
-  string FACEBOOK_URL = "https://www.facebook.com/AppsTrees";
-  string FACEBOOK_PAGE_ID = "AppsTrees";*/
+  private string WEBSITE = "https://AppsPlantingTrees.org";
+  private string TWITTER = "https://twitter.com/AppsTrees";
+  private string FACEBOOK = "https://www.facebook.com/AppsTrees";
+
 
   public void HideAboutMenu()
   {
-    GameObject aboutMenu = GameObject.FindWithTag("MenuAbout");
-    aboutMenu.SetActive(false);
+    gameObject.SetActive(false);
   }
 
   public void GoToOurTwitter()
   {
-      Debug.Log("Going to twitter...");
-      //Intent intent = new Intent(Intent.ACTION_VIEW);
-      //intent.setData(Uri.parse(TWITTER_PAGE_ID));
-      //startActivity(intent);
+    Application.OpenURL(TWITTER);
   }
 
   public void GoToOurFB()
   {
-    Debug.Log("Going to FB...");
-    /*Intent fbIntent = new Intent(Intent.ACTION_VIEW);
-    String fbUrl;
-    PackageManager packageManager = this.getPackageManager();
-        try {
-              int versionCode = packageManager.getPackageInfo("com.facebook.katana", 0).versionCode;
-              if (packageManager.getApplicationInfo("com.facebook.katana", 0).enabled) {
-                  if (versionCode >= 3002850) { //newer versions of fb app
-                      fbUrl = "fb://facewebmodal/f?href=" + FACEBOOK_URL;
-                  } else {
-                      fbUrl = "fb://page/" + FACEBOOK_PAGE_ID;
-                  }
-              } else {
-                  fbUrl = FACEBOOK_URL;
-              }
-            } catch (PackageManager.NameNotFoundException e) {
-                fbUrl = FACEBOOK_URL;
-            }
-            fbIntent.setData(Uri.parse(fbUrl));
-        startActivity(fbIntent);*/
+    Application.OpenURL(FACEBOOK);
   }
 
   public void GoToOurWebsite()
   {
-    Debug.Log("Going to web...");
-    //Intent intent = new Intent(Intent.ACTION_VIEW);
-    //intent.setData(Uri.parse(OUR_WEBSITE));
-    //startActivity(intent);
+    Application.OpenURL(WEBSITE);
   }
 
 }
