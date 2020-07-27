b507c7372ef0c6ae39c09c641350c3fae739b263
randomUsername15 <63813435+randomUsername15@users.noreply.github.com>
2020-07-27 13:35:22 +0200

    Update loading.cs

--- Assets/ManagingGdprConsent/loading.cs
+++ Assets/ManagingGdprConsent/loading.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 public class loading : MonoBehaviour
 {
-    [Obsolete]
     private void Start()
     {
-        var consentInt = PlayerPrefs.GetInt("result_gdpr", 0);
-        var consent = consentInt != 0;
-        Application.LoadLevel(consent ? "main" : "GDPR");
+        bool wasConsentReceived = false;
+        int cons = PlayerPrefs.GetInt("result_gdpr", 0);
+        if (cons == 1) wasConsentReceived = true;
+        SceneManager.LoadScene(wasConsentReceived ? "main" : "GDPR");
     }
 }
