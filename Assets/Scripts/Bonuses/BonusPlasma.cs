9aa9a1f87c7567b5c581e2fbde6b7a3797a7576c
randomUsername15 <63813435+randomUsername15@users.noreply.github.com>
2020-07-31 13:12:00 +0200

    Update BonusPlasma.cs
    Fixed resizing of the ball if ball was plasmaBall already.

--- Assets/Scripts/Bonuses/BonusPlasma.cs
+++ Assets/Scripts/Bonuses/BonusPlasma.cs
@@ -11,13 +11,22 @@ public class BonusPlasma : Bonus
        //makes all balls plasma
        //start of plasma ball make all bricks cuttable
        GameObject[] allBalls = GameObject.FindGameObjectsWithTag("Ball");
-       foreach(GameObject ball in allBalls)
-       {
-         GameObject obj = Instantiate(plasmaBall, new Vector2(ball.transform.position.x, ball.transform.position.y), Quaternion.identity);
-         obj.GetComponent<Rigidbody2D>().velocity = ball.GetComponent<Rigidbody2D>().velocity;
-         Vector3 scale = new Vector3(ball.gameObject.transform.localScale.x*130, ball.gameObject.transform.localScale.y*130, 1);
-         obj.gameObject.transform.localScale = scale;
-         Destroy(ball.gameObject);
+       BallPlasma pb = (BallPlasma)FindObjectOfType(typeof(BallPlasma));
+       if (!pb) {
+        foreach(GameObject ball in allBalls)
+        {
+          GameObject obj = Instantiate(plasmaBall, new Vector2(ball.transform.position.x, ball.transform.position.y), Quaternion.identity);
+          obj.GetComponent<Rigidbody2D>().velocity = ball.GetComponent<Rigidbody2D>().velocity;
+          Vector3 scale = new Vector3(ball.gameObject.transform.localScale.x*130, ball.gameObject.transform.localScale.y*130, 1);
+          obj.gameObject.transform.localScale = scale;
+          Destroy(ball.gameObject);
+        }
+       } else {
+          float d = PlayerPrefs.GetFloat("PlasmaBallDuration", 5f);
+          foreach(GameObject ball in allBalls)
+          {
+            ball.GetComponent<BallPlasma>().plasmaBallDuration = d;
+          }
        }
    }
 }
