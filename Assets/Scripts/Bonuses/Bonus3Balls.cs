a836f4e03a52b0f018d0974b021220c2a4eddbe4
randomUsername15 <63813435+randomUsername15@users.noreply.github.com>
2020-07-31 09:54:47 +0200

    Update Bonus3Balls.cs
    Fixed speed for new balls, added scale.

--- Assets/Scripts/Bonuses/Bonus3Balls.cs
+++ Assets/Scripts/Bonuses/Bonus3Balls.cs
@@ -6,7 +6,6 @@ public class Bonus3Balls : Bonus
 {
   public GameObject ballCopy;
   public GameObject plasmaBallCopy;
-  float speed = 100.0f;
 
    public override void GetBonus()
    {
@@ -16,18 +15,24 @@ public class Bonus3Balls : Bonus
        if (!pb) {
          foreach(GameObject ball in allBalls)
          {
+            Vector3 scale = new Vector3(ball.gameObject.transform.localScale.x, ball.gameObject.transform.localScale.y, 1);
             GameObject obj = Instantiate(ballCopy, new Vector2(ball.transform.position.x, ball.transform.position.y), Quaternion.identity);
-            obj.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 10);
+            obj.GetComponent<Rigidbody2D>().velocity = new Vector2(100, 50);
+            obj.gameObject.transform.localScale = scale;
             GameObject obj2 = Instantiate(ballCopy, new Vector2(ball.transform.position.x, ball.transform.position.y), Quaternion.identity);
-            obj2.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, speed);
+            obj2.GetComponent<Rigidbody2D>().velocity = new Vector2(50, 100);
+            obj2.gameObject.transform.localScale = scale;
          }
        } else {
          foreach(GameObject ball in allBalls)
          {
+           Vector3 scale = new Vector3(ball.gameObject.transform.localScale.x, ball.gameObject.transform.localScale.y, 1);
             GameObject obj = Instantiate(plasmaBallCopy, new Vector2(ball.transform.position.x, ball.transform.position.y), Quaternion.identity);
-            obj.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 10);
+            obj.GetComponent<Rigidbody2D>().velocity = new Vector2(100, 50);
+            obj.gameObject.transform.localScale = scale;
             GameObject obj2 = Instantiate(plasmaBallCopy, new Vector2(ball.transform.position.x, ball.transform.position.y), Quaternion.identity);
-            obj2.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, speed);
+            obj2.GetComponent<Rigidbody2D>().velocity = new Vector2(50, 100);
+            obj2.gameObject.transform.localScale = scale;
           }
        }
    }
