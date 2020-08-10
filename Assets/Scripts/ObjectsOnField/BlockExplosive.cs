e78d39bbbdae2bbe72ede1a418130b2798cd5341
randomUsername15 <63813435+randomUsername15@users.noreply.github.com>
2020-08-10 11:47:49 +0200

    Added exploding blocks

---
+++ Assets/Scripts/ObjectsOnField/BlockExplosive.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockExplosive : Block
+{
+  public GameObject explosion;
+  
+  public override void OnHit() 
+  {
+      GameObject.FindWithTag("GameManager").GetComponent<GameManager>().CountBlock();
+      GameObject.FindWithTag("CanvasGameInfo").GetComponent<CanvasGameInfo>().UpdateScore(SCORE_FOR_BLOCK);
+
+      Instantiate(explosion, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+
+      Destroy(gameObject);
+  }
+}
