e101ff70897808f9b5da5dd88ecb1cb4a9a3fbe5
randomUsername15 <63813435+randomUsername15@users.noreply.github.com>
2020-08-18 11:11:53 +0200

    Update PutBlocksOnField.cs
    New functions to place blocks

--- Assets/Scripts/PutBlocksOnField.cs
+++ Assets/Scripts/PutBlocksOnField.cs
@@ -21,33 +21,35 @@ public class PutBlocksOnField : MonoBehaviour
                             -1, -1, -1, -1, -1, -1, -1,
                             -1, -1, -1, -1, -1, -1, -1,
                             -1, -1, -1, -1, -1, -1, -1 };*/
-    private int[] map={     0, 5, 2, -1, 2, -1, 0,
-                            0, -1, 2, -1, 2, 12, 0,
-                            0, -1, 2, -1, 2, -1, 0,
-                            0, -1, 2, 9, 2, -1, 0,
-                            0, 15, 2, -1, 2, -1, 0,
-                            0, -1, 2, -1, 2, -1, 0,
-                            0, 5, 2, -1, 2, 9, 0,
-                            0, -1, 2, -1, 2, 5, 0,
-                            0, -1, 2, 12, 2, -1, 0,
-                            0, 9, 2, -1, 2, -1, 0,
-                            0, -1, 2, -1, 2, 15, 0,
-                            0, -1, 2, 9, 2, -1, 0, };
+    private int[] map={     -1, -1, -1, -1, -1, -1, -1,
+                            -1, -1, -1, -1, -1, -1, -1,
+                            -1,  3,  3, -1,  3,  3, -1,
+                            -1,  0,  3,  3,  3,  0, -1,
+                            -1,  0,  3,  2,  3,  0, -1,
+                            -1,  0,  0,  2,  0,  0, -1,
+                             0,  0,  0,  0,  0,  0,  0,
+                            -1, 14,  0, 11,  0, 14, -1,
+                            -1, -1,  0, 11,  0, -1, -1,
+                            -1, -1, -1,  0, 0, -1, -1,
+                            -1, -1, -1,  0, -1, -1, -1,
+                             6,  6,  6,  6,  6,  6,  6,
+                            -1, -1, -1, -1, -1, -1, -1  };
 
     public Block[] blocksArray;
     private int xLen = 18, yLen = 8;
-    private int xSpaceLen = 2, ySpaceLen = 2; //space between blocks, 2 2 default
+    private int xSpaceLen = 1, ySpaceLen = 1; //space between blocks, 2 2 default
     private int xStart = -60, yStart = -40;
     private int xPos, yPos;
 
     void Start()
     {
-        PutBlocks();
-        /*PutBlocksInCircle(16, 1, 60f);
-        PutBlocksInCircle(13, 5, 50f);
+        //PutBlocks();
+        //PutBlocksInCircle(16, 1, 60f);
+        /*PutBlocksInCircle(13, 5, 50f);
         PutBlocksInCircle(10, 0, 39f);
         PutBlocksInCircle(7, 0, 27f);
         PutBlocksInCircle(4, 4, 14f);*/
+        PutBlocksInRectangle(3, 12, 1);
     }
 
     public void PutBlocks()
@@ -72,6 +74,49 @@ public class PutBlocksOnField : MonoBehaviour
         }
     }
 
+    public void PutBlocksInWave(int h, int w, int typeOfBlocks)
+    {
+        xPos = xStart;
+        yPos = yStart;
+        int lineCounter = 0;
+        for (int i = h*w; i > 0; i--)
+        {
+            Instantiate(blocksArray[typeOfBlocks], new Vector2(xPos, yPos), Quaternion.identity);
+            xPos += (xLen + xSpaceLen);
+            if (i%2 == 0) {
+                yPos += 5;
+            } else {
+                yPos -= 5;
+            }
+            lineCounter++;
+            if (lineCounter >= 7) 
+            {
+                xPos = xStart;
+                yPos += (yLen + ySpaceLen);
+                lineCounter = 0;
+            }
+        }
+    }
+
+    public void PutBlocksInRectangle(int h, int w, int typeOfBlocks)
+    {
+        int xPos = xStart;
+        int yPos = yStart;
+        int lineCounter = 0;
+        for (int i = h*w; i > 0; i--)
+        {
+            Instantiate(blocksArray[typeOfBlocks], new Vector2(xPos, yPos), Quaternion.identity);
+            xPos += (xLen + xSpaceLen);
+            lineCounter++;
+            if (lineCounter >= w) 
+            {
+                xPos = xStart;
+                yPos += (yLen + ySpaceLen);
+                lineCounter = 0;
+            }
+        }
+    }
+
     public void PutBlocksInCircle(int amountOfBlocks, int typeOfBlocks, float radius)
     {
         float rotation = 90;
@@ -84,5 +129,4 @@ public class PutBlocksOnField : MonoBehaviour
             rotation += 90 * 4f / amountOfBlocks ;
         }
     }
-
 }
